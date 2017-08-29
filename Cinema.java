e6ed11b40d7909aa564b1d6fa70f82f00fcdcefa
Vasutorn S <vasutorn.s@ku.th>
2017-08-29 11:09:05 +0700

    add tickets hold time and debug global param

--- Cinema.java
+++ Cinema.java
@@ -9,10 +9,8 @@ class Person implements Runnable {
    private Thread t;
    private String threadName;
    Random rand = new Random();
-   private Cinemas c;
    
-   Person( String name,Cinemas k1) {
-      this.c = k1;
+   Person( String name) {
       threadName = name;
       System.out.println("Creating " +  threadName );
    }
@@ -20,23 +18,40 @@ class Person implements Runnable {
    public void run() {
       System.out.println("Running " +  threadName );
       try {
-          while(true){
-              int RandCinema = rand.nextInt(5);
-              int RandTicket = rand.nextInt(5)+1;
-              int a = (int)(Math.random()*500);
-              Thread.sleep(a);
-              if(Math.random() < 0.5)
-              {
-                  if(c.seat[RandCinema] + RandTicket <= 20)
-                  {
-                      c.seat[RandCinema] += RandTicket;
-                      for(int i = 0; i < 5; i++)
-                      {
-                        System.out.println("Cinema" + (i+1) + ":" + c.seat[i]);
-                      }
-                  }
-              }
-          }
+            while(true){
+                int RandCinema = rand.nextInt(5);
+                int RandTicket = rand.nextInt(5)+1;
+                int a = (int)(Math.random()*50000);
+                if(Cinema.k1.seat[RandCinema] + RandTicket <= 20)
+                {
+                    Cinema.k1.seat[RandCinema] += RandTicket;
+                    System.err.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Hold from:" + threadName);
+  //                  System.out.println(a);
+                    if(a >= 30000){
+                        Thread.sleep(30000);
+                        Cinema.k1.seat[RandCinema] -= RandTicket;
+                        Thread.sleep(a-30000);
+                        System.out.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Cancle from:" + threadName);
+                    }
+                    else 
+                    {
+                        Thread.sleep(a);
+                        if(Math.random() < 0.5)
+                        {
+                            Cinema.k1.seat[RandCinema] -= RandTicket;
+                            System.out.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Cancle from:" + threadName); 
+                        }
+                        else
+                        {
+                            System.out.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Buy from:" + threadName);
+                        }
+                    }
+                }
+                for(int i = 0 ;i < 5;i++)
+                {
+                    System.out.println("Cinema" + (i+1) + ":" + Cinema.k1.seat[i]);
+                }
+            }
       }catch (InterruptedException e) {
          System.out.println("Thread " +  threadName + " interrupted.");
       }
@@ -53,23 +68,20 @@ class Person implements Runnable {
 }
 
 public class Cinema{
-
+   public static Cinemas k1;
+   
    public static void main(String args[]) {
       
-      Cinemas k1 = new Cinemas();
-      Person R1 = new Person( "Thread-1",k1);
+      k1 = new Cinemas();
+      Person R1 = new Person( "Thread-1");
+      Person R2 = new Person( "Thread-2");
+      Person R3 = new Person( "Thread-3");
+      Person R4 = new Person( "Thread-4");
+      Person R5 = new Person( "Thread-5");
       R1.start();
-      
-      Person R2 = new Person( "Thread-2",k1);
       R2.start();
-      
-      Person R3 = new Person( "Thread-3",k1);
       R3.start();
-      
-      Person R4 = new Person( "Thread-4",k1);
       R4.start();
-      
-      Person R5 = new Person( "Thread-5",k1);
       R5.start();
       
    }   
