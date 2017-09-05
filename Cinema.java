05d0e77974b1d8e0fb57e3ae870e2ade0761dc31
Vasutorn S <vasutorn.s@ku.th>
2017-09-05 22:15:46 +0700

    rewrite and add synchronized

--- Cinema.java
+++ Cinema.java
@@ -3,6 +3,20 @@ package Cinema;
 import java.util.Random;
 class Cinemas {
     public int[] seat = new int [5];
+    
+    public synchronized boolean reserve(int number,int ticket)
+    {
+        if(this.seat[number]+ ticket <= 20)
+        {
+            this.seat[number] += ticket;
+        }
+        return this.seat[number] + ticket <= 20;
+    }
+    
+    public synchronized void refund(int number,int ticket)
+    {
+        this.seat[number] -= ticket;
+    }
 }
 
 class Person implements Runnable {
@@ -21,16 +35,15 @@ class Person implements Runnable {
             while(true){
                 int RandCinema = rand.nextInt(5);
                 int RandTicket = rand.nextInt(5)+1;
-                int a = (int)(Math.random()*50000);
-                if(Cinema.k1.seat[RandCinema] + RandTicket <= 20)
+                int a = (int)(Math.random()*5000);
+                if(Cinema.k1.reserve(RandCinema,RandTicket))
                 {
-                    Cinema.k1.seat[RandCinema] += RandTicket;
                     System.err.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Hold from:" + threadName);
   //                  System.out.println(a);
-                    if(a >= 30000){
-                        Thread.sleep(30000);
-                        Cinema.k1.seat[RandCinema] -= RandTicket;
-                        Thread.sleep(a-30000);
+                    if(a >= 3000){
+                        Thread.sleep(3000);
+                        Cinema.k1.refund(RandCinema,RandTicket);
+                        Thread.sleep(a-3000);
                         System.out.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Cancle from:" + threadName);
                     }
                     else 
@@ -38,7 +51,7 @@ class Person implements Runnable {
                         Thread.sleep(a);
                         if(Math.random() < 0.5)
                         {
-                            Cinema.k1.seat[RandCinema] -= RandTicket;
+                            Cinema.k1.refund(RandCinema,RandTicket);
                             System.out.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Cancle from:" + threadName); 
                         }
                         else
@@ -46,6 +59,10 @@ class Person implements Runnable {
                             System.out.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Buy from:" + threadName);
                         }
                     }
+                }
+                else
+                {
+                    System.err.println(RandTicket + " Tickets in Cinema " + (RandCinema+1) + " are Soldout Can not sell to :" + threadName);
                 }
                 for(int i = 0 ;i < 5;i++)
                 {
